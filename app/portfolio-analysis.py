1d1217df47f7232c36317d57b92be9f5a6284b76
btr260 <65568934+btr260@users.noreply.github.com>
2020-06-25 13:44:40 -0400

    turn data pull code into function

--- app/portfolio-analysis.py
+++ app/portfolio-analysis.py
@@ -13,26 +13,6 @@ import time
 
 load_dotenv()
 api_key = os.environ.get('ALPHAVANTAGE_API_KEY')
-portfolio = [{'id': 1, 'tck': 'ABBV', 'qty': 225.000},
-         {'id': 2, 'tck': 'AZO', 'qty': 5.000},
-         {'id': 3, 'tck': 'STZ', 'qty': 100.000},
-         {'id': 4, 'tck': 'CCI', 'qty': 39.000},
-         {'id': 5, 'tck': 'CVS', 'qty': 200.000},
-         {'id': 6, 'tck': 'DE', 'qty': 26.000},
-         {'id': 7, 'tck': 'DELL', 'qty': 108.000},
-         {'id': 8, 'tck': 'ENB', 'qty': 116.000},
-         {'id': 9, 'tck': 'HD', 'qty': 22.000},
-         {'id': 10, 'tck': 'JPM', 'qty': 35.000},
-         {'id': 11, 'tck': 'KKR', 'qty': 185.000},
-         {'id': 12, 'tck': 'MPC', 'qty': 450.000},
-         {'id': 13, 'tck': 'MRK', 'qty': 72.000},
-         {'id': 14, 'tck': 'MET', 'qty': 90.000},
-         {'id': 15, 'tck': 'NXST', 'qty': 55.000},
-         {'id': 16, 'tck': 'NVT', 'qty': 60.000},
-         {'id': 17, 'tck': 'PNR', 'qty': 60.000},
-         {'id': 18, 'tck': 'RTX', 'qty': 46.000},
-         {'id': 19, 'tck': 'SNY', 'qty': 100.000}]
-
 
 # DEFINE FUNCTIONS ----------------------------------------------------------------------
 
@@ -59,113 +39,146 @@ def num_suffix(num_for_suffix):
 
     return suffix
 
-# PULL AND WRITE DATA -------------------------------------------------------------------
+def api_data_pull(portfolio):
 
-failed_tickers = []
+    failed_tickers = []
 
-tck_list = [p['tck'] for p in portfolio]
+    tck_list = [p['tck'] for p in portfolio]
 
-batch = int(len(tck_list) / 5) + (len(tck_list) % 5 > 0)
+    batch = int(len(tck_list) / 5) + (len(tck_list) % 5 > 0)
 
-print('-----------------------------------------------', flush=True)
-print(f'You have specified a portfolio of {len(tck_list)} tickers.\nThe program will now retrieve data from the Alpha Vantage API.', flush=True)
-print('-----------------------------------------------', flush=True)
-
-if len(tck_list) > 5:
-    print('-----------------------------------------------', flush=True)
-    print(f'WARNING! DUE TO NUMBER OF TICKERS IN YOUR PORTFOLIO,\nTHE DATA COLLECTION PROCESS MAY TAKE APPROXIMATELY {batch-1} MINUTES TO COMPLETE!', flush=True)
-    print('-----------------------------------------------', flush=True)
-
-for i in range(0,batch,1):
-    start = i * 5
-    end = min(len(tck_list), start + 5)
-    if i > 0:
-        time.sleep(65)
-
-    for tkr in tck_list[start:end]:
-
-        request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={tkr}&apikey={api_key}"
-        response = requests.get(request_url)
-
-        # PARSE API DATA -----------------------------------------------------------------------
-
-        parsed_response = json.loads(response.text)
-
-        error_check_list = list(parsed_response.keys())
-        error_check = error_check_list[0]
-
-        if error_check == 'Meta Data':  # IF TICKER IS ABLE TO PULL ACTUAL DATA
-
-            ## PULL LATEST CLOSE FROM DATA ------------------------------------------------------------
-
-            close_days = list(parsed_response['Monthly Adjusted Time Series'].keys())
-            latest_day = close_days[0]
-
-            ## WRITE CSV DATA ------------------------------------------------------------------------
-
-            headers = ['timestamp', 'open', 'high', 'low', 'close', 'adj close', 'volume', 'div amt']
-
-            csv_filepath = os.path.join(os.path.dirname(os.path.abspath(
-                __file__)), '..', 'data', f"{tkr}.csv")
-
-            with open(csv_filepath, 'w') as csv_file:
-                writer = csv.DictWriter(csv_file, fieldnames=headers)
-                writer.writeheader()
-
-                for k in close_days:
-                    writer.writerow({
-                        'timestamp': k,
-                        'open': parsed_response['Monthly Adjusted Time Series'][k]['1. open'],
-                        'high': parsed_response['Monthly Adjusted Time Series'][k]['2. high'],
-                        'low': parsed_response['Monthly Adjusted Time Series'][k]['3. low'],
-                        'close': parsed_response['Monthly Adjusted Time Series'][k]['4. close'],
-                        'adj close': parsed_response['Monthly Adjusted Time Series'][k]['5. adjusted close'],
-                        'volume': parsed_response['Monthly Adjusted Time Series'][k]['6. volume'],
-                        'div amt': parsed_response['Monthly Adjusted Time Series'][k]['7. dividend amount'],
-                    })
-
-            # PRINT STATUS ---------------------------------------------------------------------
-
-            print('-----------------------------------------------', flush=True)
-            print(f"DOWNLOADING DATA FOR: {tkr}", flush=True)
-            print(f"WRITING DATA TO CSV: {os.path.abspath(csv_filepath)}", flush=True)
-            print('-----------------------------------------------', flush=True)
-
-        else:  # IF TICKER NOT FOUND ON API
-
-            if error_check == "Error Message":
-                failed_tickers.append(
-                    {'ticker': tkr, 'err_type': 'Invalid API Call'})
-
-            elif error_check == "Note":
-                failed_tickers.append(
-                    {'ticker': tkr, 'err_type': 'Exceeds API Call Limit (5 per minute and 500 per day)'})
-
-            else:
-                failed_tickers.append({'ticker': tkr, 'err_type': 'Other'})
-
-    if i < (batch - 1):
-        print('-----------------------------------------------',flush=True)
-        print(f'The {i+1}{num_suffix(i+1)} Batch of Downloads is Complete!', flush=True)
-        print('Waiting 1 minute to download next batch of data', flush=True)
-        print(f'Estimated remaining download time is approximately {batch-i-1} minutes.', flush=True)
+    if len(tck_list) > 5:
         print('-----------------------------------------------', flush=True)
+        print(f'WARNING! DUE TO NUMBER OF TICKERS IN YOUR PORTFOLIO,\nTHE DATA COLLECTION PROCESS MAY TAKE APPROXIMATELY {batch-1} MINUTES TO COMPLETE!', flush=True)
+        print('-----------------------------------------------', flush=True)
+
+    for i in range(0,batch,1):
+        start = i * 5
+        end = min(len(tck_list), start + 5)
+        if i > 0:
+            time.sleep(70)
+
+        for tkr in tck_list[start:end]:
+
+            request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={tkr}&apikey={api_key}"
+            response = requests.get(request_url)
+
+            # PARSE API DATA -----------------------------------------------------------------------
+
+            #print(response.text)
+            parsed_response = json.loads(response.text)
+
+            error_check_list = list(parsed_response.keys())
+            error_check = error_check_list[0]
+
+            if error_check == 'Meta Data':  # IF TICKER IS ABLE TO PULL ACTUAL DATA
+
+                quant=[q['qty'] for q in portfolio if q['tck']==tkr][0]
+
+                ## PULL LATEST CLOSE FROM DATA ------------------------------------------------------------
+
+                close_days = list(parsed_response['Monthly Adjusted Time Series'].keys())
+
+                ## WRITE CSV DATA ------------------------------------------------------------------------
+
+                headers = ['ticker', 'qty', 'timestamp', 'close', 'adj close', 'volume', 'div amt']
+
+                csv_filepath = os.path.join(os.path.dirname(os.path.abspath(
+                    __file__)), '..', 'data', f"{tkr}.csv")
+
+                with open(csv_filepath, 'w') as csv_file:
+                    writer = csv.DictWriter(csv_file, fieldnames=headers)
+                    writer.writeheader()
+
+                    for k in close_days:
+
+                        writer.writerow({
+                            'ticker': tkr,
+                            'qty': quant,
+                            'timestamp': k,
+                            'close': parsed_response['Monthly Adjusted Time Series'][k]['4. close'],
+                            'adj close': parsed_response['Monthly Adjusted Time Series'][k]['5. adjusted close'],
+                            'volume': parsed_response['Monthly Adjusted Time Series'][k]['6. volume'],
+                            'div amt': parsed_response['Monthly Adjusted Time Series'][k]['7. dividend amount']
+                        })
+
+                # PRINT STATUS ---------------------------------------------------------------------
+
+                print('-----------------------------------------------', flush=True)
+                print(f"DOWNLOADING DATA FOR: {tkr}", flush=True)
+                print(f"WRITING DATA TO CSV: {os.path.abspath(csv_filepath)}", flush=True)
+                print('-----------------------------------------------', flush=True)
+
+            else:  # IF TICKER NOT FOUND ON API
+
+                if error_check == "Error Message":
+                    failed_tickers.append(
+                        {'ticker': tkr, 'err_type': 'Invalid API Call'})
+
+                elif error_check == "Note":
+                    failed_tickers.append(
+                        {'ticker': tkr, 'err_type': 'Exceeds API Call Limit (5 per minute and 500 per day)'})
+
+                else:
+                    failed_tickers.append({'ticker': tkr, 'err_type': 'Other'})
+
+        if i < (batch - 1):
+            print('-----------------------------------------------',flush=True)
+            print(f'The {i+1}{num_suffix(i+1)} Batch of Downloads is Complete!', flush=True)
+            print('Waiting 1 minute to download next batch of data', flush=True)
+            print(f'Estimated remaining download time is approximately {batch-i-1} minutes.', flush=True)
+            print('-----------------------------------------------', flush=True)
+
+        else:
+            print('-----------------------------------------------', flush=True)
+            print('Data download complete', flush=True)
+            print('-----------------------------------------------', flush=True)
+
+    # ERROR SUMMARY -----------------------------------------------------------------
+    if len(failed_tickers) > 0:
+        if len(failed_tickers) == len(tck_list):
+            print("-------------------------")
+            print("UNABLE TO GENERATE REPORT FOR THE SPECIFIED TICKER(S).\nSEE ERROR SUMMARY")
+            print("-------------------------")
+
+        print("-------------------------")
+        print("ERROR SUMMARY:")
+        print("The program was unable to pull data from the API for the following ticker(s):")
+        for t in failed_tickers:
+            print(f"----{t['ticker']}: {t['err_type']}")
+        print("Please check the accuracy of the ticker(s) and try again.")
+
+        return False
 
     else:
-        print('-----------------------------------------------', flush=True)
-        print('Data download complete', flush=True)
-        print('-----------------------------------------------', flush=True)
+        return True
 
-# ERROR SUMMARY -----------------------------------------------------------------
-if len(failed_tickers) > 0:
-    if len(failed_tickers) == len(tck_list):
-        print("-------------------------")
-        print("UNABLE TO GENERATE REPORT FOR THE SPECIFIED TICKER(S).\nSEE ERROR SUMMARY")
-        print("-------------------------")
+if __name__=='__main__':
 
-    print("-------------------------")
-    print("ERROR SUMMARY:")
-    print("The program was unable to pull data from the API for the following ticker(s):")
-    for t in failed_tickers:
-        print(f"----{t['ticker']}: {t['err_type']}")
-    print("Please check the accuracy of the ticker(s) and try again.")
+    portfolio = [{'id': 1, 'tck': 'ABBV', 'qty': 225.000},
+                 {'id': 2, 'tck': 'AZO', 'qty': 5.000},
+                 {'id': 3, 'tck': 'STZ', 'qty': 100.000},
+                 {'id': 4, 'tck': 'CCI', 'qty': 39.000},
+                 {'id': 5, 'tck': 'CVS', 'qty': 200.000},
+                 {'id': 6, 'tck': 'DE', 'qty': 26.000},
+                 {'id': 7, 'tck': 'DELL', 'qty': 108.000},
+                 {'id': 8, 'tck': 'ENB', 'qty': 116.000},
+                 {'id': 9, 'tck': 'HD', 'qty': 22.000},
+                 {'id': 10, 'tck': 'JPM', 'qty': 35.000},
+                 {'id': 11, 'tck': 'KKR', 'qty': 185.000},
+                 {'id': 12, 'tck': 'MPC', 'qty': 450.000},
+                 {'id': 13, 'tck': 'MRK', 'qty': 72.000},
+                 {'id': 14, 'tck': 'MET', 'qty': 90.000},
+                 {'id': 15, 'tck': 'NXST', 'qty': 55.000},
+                 {'id': 16, 'tck': 'NVT', 'qty': 60.000},
+                 {'id': 17, 'tck': 'PNR', 'qty': 60.000},
+                 {'id': 18, 'tck': 'RTX', 'qty': 46.000},
+                 {'id': 19, 'tck': 'SNY', 'qty': 100.000}]
+
+
+    print('-----------------------------------------------', flush=True)
+    print(f'You have specified a portfolio of {len(portfolio)} tickers.\nThe program will now retrieve data from the Alpha Vantage API.', flush=True)
+    print('-----------------------------------------------', flush=True)
+
+    pull = api_data_pull(portfolio)
+    #print(pull)
