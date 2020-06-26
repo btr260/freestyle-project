c445fe8d973b23e25a5b84b6ae4ef24dd28206ad
btr260 <65568934+btr260@users.noreply.github.com>
2020-06-26 12:40:12 -0400

    refined return code and prep for standard deviation

--- app/port-data-analysis.py
+++ app/port-data-analysis.py
@@ -78,7 +78,7 @@ sub = full_sort.loc[(full_sort['month'] <= minomax) & (full_sort['month'] >= max
 sub = sub.sort_values(by=['ticker', 'month'])
 
 
-# CALCULATE RETURNS (this and other functions from https://www.codingfinance.com/post/2018-04-03-calc-returns-py/)
+# CALCULATE RETURNS (this and others from https://www.codingfinance.com/post/2018-04-03-calc-returns-py/)
 
 sub['mret'] = sub.groupby('ticker')['adj close'].pct_change()
 sub['mretp1'] = sub['mret'] + 1
@@ -93,25 +93,35 @@ pd_start = max(minomax - (pd_len * 12), maxomin)
 # Start Value of Assets
 pd_start_val = sub.loc[sub['month']==pd_start]
 pd_start_val = pd_start_val.set_index('ticker')
-pd_start_val=pd_start_val['sh val']
+pd_start_val=pd_start_val['sh val'].rename('start val')
 
 # Cum Return of Assets
 cum_ret_set = sub.loc[(sub['month'] > pd_start) & (sub['month'] <= pd_end)]
 cum_ret_set = cum_ret_set.set_index('ticker')
-cum_ret_set['cumret']=cum_ret_set.groupby('ticker')['mretp1'].cumprod()
-cum_ret_set = cum_ret_set.loc[cum_ret_set['month'] == pd_end]
-cum_ret = cum_ret_set['cumret']
+cum_ret_set['cumret'] = cum_ret_set.groupby('ticker')['mretp1'].cumprod()
+cum_ret_set = cum_ret_set.join(pd_start_val, on='ticker')
+cum_ret_set['mon val'] = cum_ret_set['start val'] * cum_ret_set['cumret']
+
+port_ret = cum_ret_set.groupby('month')[['start val', 'mon val']].sum()
+port_ret['cum ret'] = port_ret['mon val'] / port_ret['start val']
+port_ret['mon ret'] = port_ret['mon val'].pct_change()
+
+port_ret.loc[pd_start + 1, 'mon ret'] = port_ret.loc[pd_start + 1, 'cum ret'] - 1
 
 # Calc Portf Return
-pd_end_val = pd_start_val * cum_ret
-port_start_val = pd_start_val.sum()
-port_end_val = pd_end_val.sum()
-tot_pd_ret = port_end_val / port_start_val - 1
-months = 12 * (pd_end.year - pd_start.year) + (pd_end.month - pd_start.month)
+tot_pd_ret = port_ret.loc[pd_end,'cum ret'] - 1
+months = len(port_ret)
 years = months / 12
 avg_ann_ret = (1 + tot_pd_ret)**(1 / years) - 1
 avg_mon_ret = (1 + tot_pd_ret)**(1 / months) - 1
 
-ret_calc = {'years_tgt': pd_len, 'years_act': years, 'st_date': pd_start, 'end_date': pd_end, 'ann_ret': avg_ann_ret, 'mon_ret': avg_mon_ret}
+
+breakpoint()
+
+#Calc Return Std Dev
+mon_sdev = cum_ret_set.groupby('ticker')['mret'].std()
+ann_sdev = mon_sdev * (12 ** .5)
+
+ret_calc = {'years_tgt': pd_len, 'years_act': years, 'st_date': pd_start, 'end_date': pd_end, 'ann_ret': avg_ann_ret, 'mon_ret': avg_mon_ret,'ann_sdev':ann_sdev, 'mon_sdev':mon_sdev}
 
 breakpoint()
