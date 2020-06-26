9bc5b180e54b9d5e95081d699c2a73f1907812e4
btr260 <65568934+btr260@users.noreply.github.com>
2020-06-26 13:25:11 -0400

    made the return calcs a function

--- app/port-data-analysis.py
+++ app/port-data-analysis.py
@@ -5,6 +5,61 @@ from dotenv import load_dotenv
 import datetime
 import numpy as np
 
+# -------------------------------------------------------------------------------------
+# FUNCTIONS ---------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
+
+def returns(dataset,period_length,min_start,max_end):
+
+    working_data = dataset
+    working_data['mret'] = working_data.groupby('ticker')['adj close'].pct_change()
+    working_data['mretp1'] = working_data['mret'] + 1
+
+    working_data['sh val'] = working_data['qty'] * working_data['close']
+
+    pd_len = period_length
+    pd_end = max_end
+    pd_start = max(max_end - (pd_len * 12), min_start)
+
+    # Start Value of Assets
+    pd_start_val = working_data.loc[working_data['month'] == pd_start]
+    pd_start_val = pd_start_val.set_index('ticker')
+    pd_start_val = pd_start_val['sh val'].rename('start val')
+
+    # Cum Return of Assets
+    cum_ret_set = working_data.loc[(working_data['month'] > pd_start) & (working_data['month'] <= pd_end)]
+    cum_ret_set = cum_ret_set.set_index('ticker')
+    cum_ret_set['cumret'] = cum_ret_set.groupby('ticker')['mretp1'].cumprod()
+    cum_ret_set = cum_ret_set.join(pd_start_val, on='ticker')
+    cum_ret_set['mon val'] = cum_ret_set['start val'] * cum_ret_set['cumret']
+
+    port_ret = cum_ret_set.groupby('month')[['start val', 'mon val']].sum()
+    port_ret['cum ret'] = port_ret['mon val'] / port_ret['start val']
+    port_ret['mon ret'] = port_ret['mon val'].pct_change()
+
+    port_ret.loc[pd_start + 1,
+                 'mon ret'] = port_ret.loc[pd_start + 1, 'cum ret'] - 1
+
+    # Calc Portf Return
+    tot_pd_ret = port_ret.loc[pd_end, 'cum ret'] - 1
+    months = len(port_ret)
+    years = months / 12
+    avg_ann_ret = (1 + tot_pd_ret)**(1 / years) - 1
+    avg_mon_ret = (1 + tot_pd_ret)**(1 / months) - 1
+
+    #Calc Return Std Dev
+    mon_sdev = port_ret['mon ret'].std()
+    ann_sdev = mon_sdev * (12 ** .5)
+
+    ret_calc = {'years_tgt': pd_len, 'years_act': years, 'months_act': months, 'st_date': f'{pd_start.year}-{pd_start.month}',
+                'end_date': f'{pd_end.year}-{pd_end.month}', 'ann_ret': avg_ann_ret, 'mon_ret': avg_mon_ret, 'ann_sdev': ann_sdev, 'mon_sdev': mon_sdev}
+
+    return ret_calc
+
+# -------------------------------------------------------------------------------------
+# CODE --------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
+
 portfolio = [{'id': 1, 'tck': 'ABBV', 'qty': 225.000},
              {'id': 2, 'tck': 'AZO', 'qty': 5.000},
              {'id': 3, 'tck': 'STZ', 'qty': 100.000},
@@ -25,6 +80,9 @@ portfolio = [{'id': 1, 'tck': 'ABBV', 'qty': 225.000},
 
 
 tck_list = [t['tck'] for t in portfolio]
+
+
+# Import saved data and combine --------------------------------------------------------
 
 start_path = os.path.join(os.path.dirname(os.path.abspath(
     __file__)), '..', 'data', f"{tck_list[0]}.csv")
@@ -59,8 +117,7 @@ full_sort = full.sort_values(by=['ticker', 'timestamp'])
 # CREATE MONTH VARIABLE
 full_sort['month'] = full_sort['timestamp'].dt.to_period('M')  # SOURCE: https://stackoverflow.com/questions/45304531/extracting-the-first-day-of-month-of-a-datetime-type-column-in-pandas
 
-
-# GET FIRST AND LAST MONTHS
+# Resize data for consistent periods -------------------------------------------------------
 
 maxomin = full_sort.groupby('ticker')['month'].min()
 #print(maxomin)
@@ -78,50 +135,8 @@ sub = full_sort.loc[(full_sort['month'] <= minomax) & (full_sort['month'] >= max
 sub = sub.sort_values(by=['ticker', 'month'])
 
 
-# CALCULATE RETURNS (this and others from https://www.codingfinance.com/post/2018-04-03-calc-returns-py/)
+# CALCULATE RETURNS
+results = []
 
-sub['mret'] = sub.groupby('ticker')['adj close'].pct_change()
-sub['mretp1'] = sub['mret'] + 1
-
-sub['sh val'] = sub['qty'] * sub['close']
-
-pd_len = 5
-pd_end = minomax
-pd_start = max(minomax - (pd_len * 12), maxomin)
-
-
-# Start Value of Assets
-pd_start_val = sub.loc[sub['month']==pd_start]
-pd_start_val = pd_start_val.set_index('ticker')
-pd_start_val=pd_start_val['sh val'].rename('start val')
-
-# Cum Return of Assets
-cum_ret_set = sub.loc[(sub['month'] > pd_start) & (sub['month'] <= pd_end)]
-cum_ret_set = cum_ret_set.set_index('ticker')
-cum_ret_set['cumret'] = cum_ret_set.groupby('ticker')['mretp1'].cumprod()
-cum_ret_set = cum_ret_set.join(pd_start_val, on='ticker')
-cum_ret_set['mon val'] = cum_ret_set['start val'] * cum_ret_set['cumret']
-
-port_ret = cum_ret_set.groupby('month')[['start val', 'mon val']].sum()
-port_ret['cum ret'] = port_ret['mon val'] / port_ret['start val']
-port_ret['mon ret'] = port_ret['mon val'].pct_change()
-
-port_ret.loc[pd_start + 1, 'mon ret'] = port_ret.loc[pd_start + 1, 'cum ret'] - 1
-
-# Calc Portf Return
-tot_pd_ret = port_ret.loc[pd_end,'cum ret'] - 1
-months = len(port_ret)
-years = months / 12
-avg_ann_ret = (1 + tot_pd_ret)**(1 / years) - 1
-avg_mon_ret = (1 + tot_pd_ret)**(1 / months) - 1
-
-
-breakpoint()
-
-#Calc Return Std Dev
-mon_sdev = cum_ret_set.groupby('ticker')['mret'].std()
-ann_sdev = mon_sdev * (12 ** .5)
-
-ret_calc = {'years_tgt': pd_len, 'years_act': years, 'st_date': pd_start, 'end_date': pd_end, 'ann_ret': avg_ann_ret, 'mon_ret': avg_mon_ret,'ann_sdev':ann_sdev, 'mon_sdev':mon_sdev}
-
-breakpoint()
+for i in [1,2,3,5]:
+    results.append(returns(sub, i, maxomin, minomax))
